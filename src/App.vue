ce62769af36b9a7643a62d5b2f3e8d3074a3d67b
jean carllo <jeanrosete04@gmail.com>
2024-01-09 11:36:39 -0300

    tentativa falha, necessario auxilio.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <script setup>
-  import 'bootstrap/dist/css/bootstrap.min.css';
-  import { reactive, ref } from 'vue';
-  
-/* 
-variaveis:
-resultante for in;
-soma: objeto;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { reactive, ref } from 'vue';
 
 
-*/
 
-  const estado = reactive ({
-    respostaTemp: (''),
-    filtro: 'adicao',
-    resultado: [ 
-      {
+const estado = reactive({
+  respostaTemp: (''),
+  filtro: 'adicao',
+  resultado: [
+    {
       solucao: '3 + 3 = 6'
     },
     {
@@ -25,63 +19,59 @@ soma: objeto;
     },
     {
       solucao: '3 ÷ 3 = 1'
-    } 
+    }
   ]
-  })
+})
 
-  const valorA = ref('');
-  const valorB = ref('');
+const valorA = ref('');
+const valorB = ref('');
 
 
-  const adicao = () => {
-    respostaTemp = valorA + valorB;
-    console.log(respostaTemp);
-  }
+const adicao = () => {
+  estado.respostaTemp = valorA + valorB;
+}
 
-  const subtracao = () => {
-    respostaTemp = valorA - valorB;
-    console.log(respostaTemp);
-  }
+const subtracao = () => {
+  estado.respostaTemp = valorA - valorB;
+}
 
-  const multiplicacao = () => {
-    respostaTemp = valorA * valorB;
-    console.log(respostaTemp);
-  }
+const multiplicacao = () => {
+  estado.respostaTemp = valorA * valorB;
+}
 
-  const divisao = () => {
-    respostaTemp = valorA / valorB;
-    console.log(respostaTemp);
-  }
-
-    const getOperacao = () => {
-      const { filtro} = estado;
-      
-      switch(filtro) {
-        case 'adicao':
-          return adicao();
-          case 'subtracao':
-            return subtracao();
-            case 'multiplicacao':
-        return multiplicacao();
-      default:
-        return divisao();
-    }
-
-  }
+const divisao = () => {
+  estado.respostaTemp = valorA / valorB;
+}
   
-  const introResposta = () => {
-    const tarefaNova = {
-      solucao: estado.respostaTemp
-    }
-    estado.resultado.push(tarefaNova);
+
+const getOperacao = () => {
+  const { filtro } = estado;
+
+  switch (filtro) {
+    case 'adicao':
+      return adicao();
+    case 'subtracao':
+      return subtracao();
+    case 'multiplicacao':
+      return multiplicacao();
+    default:
+      return divisao();
   }
+}
+
+const introResposta = () => {
+  const tarefaNova = {
+    solucao: estado.respostaTemp
+  }
+  estado.resultado.push(tarefaNova);
+}
 </script>
 
 <template>
   <header class="p-5 text-center bg-success ">
     <div class="container">
-        <h1 class="text-white">
-        Equação Rápida 
+      <h1 class="text-white">
+        Equação Rápida
       </h1>
     </div>
   </header>
@@ -89,18 +79,20 @@ soma: objeto;
     <form class="mt-4">
       <div class="row">
         <div class="col-4">
-          <input v-model="ValorA" @keyup=" evento => estado.respostaTemp = evento.target.value" class="form-control" type="number"  placeholder="digite um numero">
+          <input v-model="ValorA" @keyup="evento => estado.respostaTemp = evento.target.value" class="form-control"
+            type="number" placeholder="digite um numero">
         </div>
         <div class="col-4">
-          <input v-model="ValorB" @keyup=" evento => estado.respostaTemp = evento.target.value" class="form-control" type="number"  placeholder="digite um numero" >
+          <input v-model="ValorB" @keyup="evento => estado.respostaTemp = evento.target.value" class="form-control"
+            type="number" placeholder="digite um numero">
         </div>
         <div class="col-4">
-          <select @change="getOperacao()" class=" form-control">
-          <option value="adicao">adição</option>
-          <option value="subtracao">subtração</option>
-          <option value="multiplicacao">multiplicação</option>
-          <option value="divisao">divisão</option>
-        </select>
+          <select @keyup="getOperacao() " class=" form-control">
+            <option value="adicao">adição</option>
+            <option value="subtracao">subtração</option>
+            <option value="multiplicacao">multiplicação</option>
+            <option value="divisao">divisão</option>
+          </select>
         </div>
       </div>
     </form>
@@ -109,10 +101,7 @@ soma: objeto;
         <label for="">{{ resultante.solucao }}</label>
       </li>
     </ul>
-</div>
+  </div>
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
