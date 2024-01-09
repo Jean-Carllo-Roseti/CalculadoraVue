d1022d88125dfa3a17321b7f3d274dbc6f06c4e6
jean carllo <jeanrosete04@gmail.com>
2024-01-09 11:20:28 -0300

    dificuldade na lógica.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,118 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import { reactive, ref } from 'vue';
+  
+/* 
+variaveis:
+resultante for in;
+soma: objeto;
+
+
+*/
+
+  const estado = reactive ({
+    respostaTemp: (''),
+    filtro: 'adicao',
+    resultado: [ 
+      {
+      solucao: '3 + 3 = 6'
+    },
+    {
+      solucao: '3 - 3 = 0'
+    },
+    {
+      solucao: '3 x 3 = 9'
+    },
+    {
+      solucao: '3 ÷ 3 = 1'
+    } 
+  ]
+  })
+
+  const valorA = ref('');
+  const valorB = ref('');
+
+
+  const adicao = () => {
+    respostaTemp = valorA + valorB;
+    console.log(respostaTemp);
+  }
+
+  const subtracao = () => {
+    respostaTemp = valorA - valorB;
+    console.log(respostaTemp);
+  }
+
+  const multiplicacao = () => {
+    respostaTemp = valorA * valorB;
+    console.log(respostaTemp);
+  }
+
+  const divisao = () => {
+    respostaTemp = valorA / valorB;
+    console.log(respostaTemp);
+  }
+
+    const getOperacao = () => {
+      const { filtro} = estado;
+      
+      switch(filtro) {
+        case 'adicao':
+          return adicao();
+          case 'subtracao':
+            return subtracao();
+            case 'multiplicacao':
+        return multiplicacao();
+      default:
+        return divisao();
+    }
+
+  }
+  
+  const introResposta = () => {
+    const tarefaNova = {
+      solucao: estado.respostaTemp
+    }
+    estado.resultado.push(tarefaNova);
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <header class="p-5 text-center bg-success ">
+    <div class="container">
+        <h1 class="text-white">
+        Equação Rápida 
+      </h1>
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <form class="mt-4">
+      <div class="row">
+        <div class="col-4">
+          <input v-model="ValorA" @keyup=" evento => estado.respostaTemp = evento.target.value" class="form-control" type="number"  placeholder="digite um numero">
+        </div>
+        <div class="col-4">
+          <input v-model="ValorB" @keyup=" evento => estado.respostaTemp = evento.target.value" class="form-control" type="number"  placeholder="digite um numero" >
+        </div>
+        <div class="col-4">
+          <select @change="getOperacao()" class=" form-control">
+          <option value="adicao">adição</option>
+          <option value="subtracao">subtração</option>
+          <option value="multiplicacao">multiplicação</option>
+          <option value="divisao">divisão</option>
+        </select>
+        </div>
+      </div>
+    </form>
+    <ul class="list-group mt-3" v-for="resultante in estado.resultado">
+      <li class="list-group-item">
+        <label for="">{{ resultante.solucao }}</label>
+      </li>
+    </ul>
+</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
